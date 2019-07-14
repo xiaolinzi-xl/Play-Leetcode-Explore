2bc1af5812da29b57b287e9ffca9a9136fa92a3d
xiaolinzi-xl <daoshangxl@163.com>
2019-07-14 18:02:29 +0800

    爬楼梯

---
+++ algorithm/primary_algorithm/dp/70.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 3:
+            return n
+        a, b = 1, 2
+        for _ in range(2, n):
+            c = a + b
+            a = b
+            b = c
+        return b
