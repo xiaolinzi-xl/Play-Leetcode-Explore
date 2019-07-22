d7b559e99e32fc34c05a0041016f36ccfe7a9ba0
xiaolinzi-xl <daoshangxl@163.com>
2019-07-23 00:47:03 +0800

    跳跃游戏（超时）

---
+++ algorithm/intermediate_algorithm/dp/leetcode_55.py
@@ -0,0 +1,43 @@
+class Solution:
+    dp = []
+
+    def search(self, nums, idx):
+        if self.dp[idx] != -1:
+            return self.dp[idx]
+        if nums[idx] + idx >= len(nums) - 1:
+            self.dp[idx] = nums[idx] + idx
+            return self.dp[idx]
+        for step in range(1, nums[idx] + 1):
+            self.dp[idx] = max(self.dp[idx], idx + step)
+            if self.dp[idx] >= len(nums) - 1:
+                break
+            self.dp[idx] = max(self.dp[idx], self.search(nums, idx + step))
+        return self.dp[idx]
+
+    # 超时
+    def canJump(self, nums):
+        self.dp = [-1] * len(nums)
+        return self.search(nums, 0) >= len(nums) - 1
+
+    # 超时
+    def canJump_1(self, nums):
+        n = len(nums)
+        if n == 1:
+            return True
+        dp = [-1] * n
+        dp[n - 1] = n - 1
+        for i in range(n - 2, -1, -1):
+            for j in range(min(i + nums[i], n - 1), i, -1):
+                dp[i] = max(dp[i], dp[j])
+                if dp[i] == n - 1:
+                    break
+            if dp[i] == n - 1 and nums[0] >= i:
+                dp[0] = n - 1
+                break
+        return dp[0] == n - 1
+
+
+if __name__ == '__main__':
+    # nums = [2, 3, 1, 1, 4]
+    nums = [3, 2, 1, 0, 4]
+    print(Solution().canJump_1(nums))
